4d80a1448e15811c8d214f1acba7bccff9621644
VanThanh-coder <lovanthanh9124@gmail.com>
2025-03-02 13:13:44 +0700

    update

--- project/src/App.js
+++ project/src/App.js
@@ -5,14 +5,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <App1></App1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
